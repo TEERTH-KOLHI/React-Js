736cc372ca10685331d9e93ebe2c9acdc32795a7
TEERTH-KOLHI <teerth.bscsses21@iba-suk.edu.pk>
2024-07-18 00:56:04 +0500

    react vite project simple

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Component01 from "./Component01"
+function App() {
+  
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>Project 1</p>
+      <br />
+
+      <Component01/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Component01.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component01 = () => {
+  return (
+    <>
+        <h1>I am in component01</h1>
+    </>
+  )
+}
+
+export default Component01
